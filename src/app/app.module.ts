7224918dfbe8889fef9118ab7fae3c82e527abcc
joaozinsh <joaogabriel241201@gmail.com>
2021-06-28 10:39:59 -0300

    criação do componente login-cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginCadastroComponent } from './login-cadastro/login-cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginCadastroComponent
   ],
   imports: [
     BrowserModule,
