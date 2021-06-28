aefae9e4a4997a21a429b95a616857ad12435bbb
Thiago Ramaldes Santos <tramaldes.s@gmail.com>
2021-06-28 16:01:28 -0300

    Criação do componente concluir compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConcluirCompraComponent } from './concluir-compra/concluir-compra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConcluirCompraComponent
   ],
   imports: [
     BrowserModule,
