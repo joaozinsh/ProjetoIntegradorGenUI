3041192a7e5eb7f08a7a0454761157e4075724eb
ERIK-GREFF <83592613+ERIK-GREFF@users.noreply.github.com>
2021-06-28 14:39:38 -0300

    feature/criação do rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
