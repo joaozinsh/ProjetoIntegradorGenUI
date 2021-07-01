14c3b52f47497e8a456ab1cc2473d43f7b5c9d13
ERIK-GREFF <83592613+ERIK-GREFF@users.noreply.github.com>
2021-07-01 19:13:12 -0300

    Feature página produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginCadastroComponent } from './login-cadastro/login-cadastro.componen
 import { NavbarComponent } from './navbar/navbar.component';
 import { ConcluirCompraComponent } from './concluir-compra/concluir-compra.component';
 import { ContatoComponent } from './contato/contato.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ContatoComponent } from './contato/contato.component';
     ContatoComponent,
     LoginCadastroComponent,
     NavbarComponent,
-    ConcluirCompraComponent
+    ConcluirCompraComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
