61551fa713996ab8df27de35ff6f6d0b960db7b3
João Gabriel <joaogabriel241201@gmail.com>
2021-07-05 14:50:45 -0300

    Fix: sujeira no app.module; Refactor: fixando o footer na parte inferior da pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,11 @@ import { LoginCadastroComponent } from './login-cadastro/login-cadastro.componen
 import { NavbarComponent } from './navbar/navbar.component';
 import { ConcluirCompraComponent } from './concluir-compra/concluir-compra.component';
 import { ContatoComponent } from './contato/contato.component';
-<<<<<<< HEAD
 import { CarrinhoVazioComponent } from './carrinho-vazio/carrinho-vazio.component';
-
-=======
 import { ProdutosComponent } from './produtos/produtos.component';
 import { PagamentoComponent } from './pagamento/pagamento.component';
 import { SobreComponent } from './sobre/sobre.component';
-<<<<<<< HEAD
->>>>>>> 9cfc58b2ad8b7dd2739b5b79df9dce87b71c201d
-=======
 import { CarrinhoComponent } from './carrinho/carrinho.component';
->>>>>>> b5bc15a63d0b4677c755bc7ca488b825cddf3089
 
 @NgModule({
   declarations: [
@@ -33,19 +26,11 @@ import { CarrinhoComponent } from './carrinho/carrinho.component';
     LoginCadastroComponent,
     NavbarComponent,
     ConcluirCompraComponent,
-<<<<<<< HEAD
     CarrinhoVazioComponent,
-    
-=======
     ProdutosComponent,
     PagamentoComponent,
-<<<<<<< HEAD
-    SobreComponent
->>>>>>> 9cfc58b2ad8b7dd2739b5b79df9dce87b71c201d
-=======
     SobreComponent,
     CarrinhoComponent
->>>>>>> b5bc15a63d0b4677c755bc7ca488b825cddf3089
   ],
   imports: [
     BrowserModule,
